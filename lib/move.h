6c5488be4639a103574fd77c76a944743500fab0
MORI Shingo <cos@kmc.gr.jp>
2017-08-05 13:27:09 +0900

    moveでpunterを保存するようにした

--- lib/move.h
+++ lib/move.h
@@ -10,19 +10,20 @@ enum class MoveType {
 
 class Move {
 public:
-  static Move Claim(int source, int target) {
-    return Move(MoveType::kClaim, source, target);
+  static Move Claim(int punter_id, int source, int target) {
+    return Move(MoveType::kClaim, punter_id, source, target);
   }
 
-  static Move Pass() {
-    return Move(MoveType::kPass, -1, -1);
+  static Move Pass(int punter_id) {
+    return Move(MoveType::kPass, punter_id, -1, -1);
   }
 
   MoveType Type() const { return type; }
+  int PunterID() const { return punter_id; }
   int Source() const { return source; }
   int Target() const { return target; }
 
-  picojson::object SerializeJson(int punter_id) const {
+  picojson::object SerializeJson() const {
     if (type == MoveType::kClaim) {
       picojson::object j;
       j["punter"] = picojson::value((double)punter_id);
@@ -47,22 +48,26 @@ public:
   static Move Deserialize(const picojson::object& json) {
     if (json.find("claim") != json.end()) {
       auto o = json.at("claim").get<picojson::object>();
+      int punter_id = (int)o.at("punter").get<double>();
       int source = (int)o.at("source").get<double>();
       int target = (int)o.at("target").get<double>();
-      return Move::Claim(source, target);
+      return Move::Claim(punter_id, source, target);
     } else if (json.find("pass") != json.end()) {
-      return Move::Pass();
+      auto o = json.at("pass").get<picojson::object>();
+      int punter_id = (int)o.at("punter").get<double>();
+      return Move::Pass(punter_id);
     } else {
       assert(false);
     }
   }
 
 private:
-  Move(MoveType type, int source, int target):
-    type(type), source(source), target(target) {}
+  Move(MoveType type, int punter_id, int source, int target):
+    type(type), punter_id(punter_id), source(source), target(target) {}
 
 private:
   MoveType type;
+  int punter_id;
   int source;
   int target;
 };
