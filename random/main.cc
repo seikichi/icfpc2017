9ca99f6b81dfd0dfeaaccae864b6f3d92cb26a3c
Seiichi KONDO <seikichi@kmc.gr.jp>
2017-08-05 19:39:17 +0900

    Add random AI

---
+++ random/main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <memory>
+#include "protocol.h"
+
+using namespace std;
+
+int main(int, char**) {
+  auto name = "YAGI";
+  auto protocol = make_unique<OfflineClientProtocol>();
+
+  protocol->SetPlayerName(name);
+  protocol->Receive();
+
+  if (protocol->Phase() == GamePhase::kSetup) {
+    auto game = protocol->Game();
+    protocol->SetState(game.SerializeString());
+  } else if (protocol->Phase() == GamePhase::kGamePlay) {
+    Game game;
+    game.Deserialize(protocol->State());
+    protocol->SetPlayerMove(Move::Pass(game.PunterID()));
+    protocol->SetState(game.SerializeString());
+  } else if (protocol->Phase() == GamePhase::kScoring) {
+    for (auto score : protocol->Scores()) {
+      cerr << "PunterID = " << score.PunterID() << ", Score = " << score.Get() << endl;
+    }
+  }
+  protocol->Send();
+
+  return 0;
+}
