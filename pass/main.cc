a61975c30b92fb5cba988689e5bd4b0ebe6e66b6
Seiichi KONDO <seikichi@kmc.gr.jp>
2017-08-05 19:36:23 +0900

    Remove stderr log

--- pass/main.cc
+++ pass/main.cc
@@ -12,23 +12,18 @@ int main(int, char**) {
   protocol->Receive();
 
   if (protocol->Phase() == GamePhase::kSetup) {
-    cerr << "Setup" << endl;
     auto game = protocol->Game();
-    cerr << game.SerializeString() << endl;
     protocol->SetState(game.SerializeString());
   } else if (protocol->Phase() == GamePhase::kGamePlay) {
-    cerr << "GamePlay: state = " << protocol->State() << endl;
     Game game;
     game.Deserialize(protocol->State());
     protocol->SetPlayerMove(Move::Pass(game.PunterID()));
     protocol->SetState(game.SerializeString());
   } else if (protocol->Phase() == GamePhase::kScoring) {
-    cerr << "Scoring" << endl;
     for (auto score : protocol->Scores()) {
       cerr << "PunterID = " << score.PunterID() << ", Score = " << score.Get() << endl;
     }
   }
-  cerr << "Send" << endl;
   protocol->Send();
 
   return 0;
