1add0c6a94034056b4602c83c1efc3936e273cda
Seiichi KONDO <seikichi@kmc.gr.jp>
2017-08-05 19:59:40 +0900

    Add random AI

--- random/main.cc
+++ random/main.cc
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <unordered_set>
 #include <memory>
+#include <ctime>
 #include "protocol.h"
+#include "random.h"
 
 using namespace std;
 
@@ -17,7 +20,32 @@ int main(int, char**) {
   } else if (protocol->Phase() == GamePhase::kGamePlay) {
     Game game;
     game.Deserialize(protocol->State());
-    protocol->SetPlayerMove(Move::Pass(game.PunterID()));
+    auto sites = game.Map().Sites();
+
+    unordered_set<int> lambdas;
+    for (auto site : sites) {
+      if (site.is_mine) {
+        lambdas.insert(site.id);
+      }
+    }
+
+    vector<Edge> candidates;
+    for (auto edges : game.Map().Graph()) {
+      for (auto edge : edges) {
+        if (lambdas.count(edge.src) || lambdas.count(edge.dest)) {
+          candidates.push_back(edge);
+        }
+      }
+    }
+
+    Random rand(time(NULL));
+    auto edge = candidates[rand.next(0, candidates.size())];
+
+    auto src = sites[edge.src].original_id;
+    auto dest = sites[edge.dest].original_id;
+    auto move = Move::Claim(game.PunterID(), src, dest);
+
+    protocol->SetPlayerMove(move);
     protocol->SetState(game.SerializeString());
   } else if (protocol->Phase() == GamePhase::kScoring) {
     for (auto score : protocol->Scores()) {
