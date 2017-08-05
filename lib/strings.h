72735d210c008a220a29d8ab45e041b4b14f8494
qwerty <qwerty.knagira@gmail.com>
2017-08-05 10:22:37 +0900

    add header for string.h
    add cstdlib for string.h

--- lib/strings.h
+++ lib/strings.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 std::string JoinString(const std::vector<std::string>& strings,
                        const std::string& separator);
