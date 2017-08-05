b211cb6ffc0211245c0b441fb8773ad402b2b0c8
Seiichi KONDO <seikichi@kmc.gr.jp>
2017-08-05 18:13:38 +0900

    Add pass client program

---
+++ pass/main.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+#include "protocol.h"
+
+using namespace std;
+
+int main(int, char**) {
+  auto name = "YAGI";
+  auto protocol = make_unique<OfflineClientProtocol>();
+
+  protocol->SetPlayerName(name);
+  protocol->Receive();
+
+  if (protocol->Phase() == GamePhase::kSetup) {
+    cerr << "Setup" << endl;
+    auto game = protocol->Game();
+    cerr << game.SerializeString() << endl;
+    protocol->SetState(game.SerializeString());
+  } else if (protocol->Phase() == GamePhase::kGamePlay) {
+    cerr << "GamePlay" << endl;
+    Game game;
+    game.Deserialize(protocol->State());
+    protocol->SetPlayerMove(Move::Pass(game.PunterID()));
+    protocol->SetState(game.SerializeString());
+  } else if (protocol->Phase() == GamePhase::kScoring) {
+    cerr << "Scoring" << endl;
+    for (auto score : protocol->Scores()) {
+      cerr << score.PunterID() << ":" << score.Get() << endl;
+    }
+  }
+  cerr << "Send" << endl;
+  protocol->Send();
+
+  return 0;
+}
